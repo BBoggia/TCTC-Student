be30102cd5164cad00f71e21a49641d5f1f0bfa5
Branson Boggia <bmanboggia@yahoo.com>
2016-10-28 12:56:40 -0400

    10/28 another small edit

---
+++ TCTC Student/HomePageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomePageViewController.swift
+//  TCTC Student
+//
+//  Created by Branson Boggia on 10/28/16.
+//  Copyright © 2016 Pinetree Studios. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scrollView.contentInset = UIEdgeInsetsMake(0, 0, 400, 0)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
