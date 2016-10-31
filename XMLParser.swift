bbf0d6f1bea1a6bfc3c96a19df4bdb1dba136c5d
Branson Boggia <bmanboggia@yahoo.com>
2016-10-31 13:59:46 -0400

    10/31 Kinda broke star view added web view and what not for announcements use RSS

---
+++ XMLParser.swift
@@ -0,0 +1,13 @@
+//
+//  XMLParser.swift
+//  TCTC Student
+//
+//  Created by Branson Boggia on 10/31/16.
+//  Copyright © 2016 Pinetree Studios. All rights reserved.
+//
+
+import UIKit
+
+class XMLParser: NSObject {
+
+}
