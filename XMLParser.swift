060a3e091cde9c8c0b173a05ed7af6182b881f49
Branson Boggia <bmanboggia@yahoo.com>
2016-11-03 13:59:28 -0400

    11/3 added guidance and fixed errors

--- XMLParser.swift
+++ XMLParser.swift
@@ -8,6 +8,69 @@
 
 import UIKit
 
-class XMLParser: NSObject {
+import UIKit
 
+@objc protocol XMLParserDelegate{
+    func parsingWasFinished()
+}
+
+class XMLParser: NSObject, Foundation.XMLParserDelegate {
+    
+    var arrParsedData = [Dictionary<String, String>]()
+    
+    var currentDataDictionary = Dictionary<String, String>()
+    
+    var currentElement = ""
+    
+    var foundCharacters = ""
+    
+    var delegate : XMLParserDelegate?
+    
+    
+    func startParsingWithContentsOfURL(_ rssURL: URL) {
+        let parser = Foundation.XMLParser(contentsOf: rssURL)
+        parser!.delegate = self
+        parser!.parse()
+    }
+    
+    
+    
+    //MARK: NSXMLParserDelegate method implementation
+    
+    private func parserDidEndDocument(_ parser: XMLParser) {
+        delegate?.parsingWasFinished()
+    }
+    
+    
+    private func parser(_ parser: Foundation.XMLParser, didStartElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!, attributes attributeDict: [AnyHashable: Any]) {
+        
+        currentElement = elementName
+    }
+    
+    
+    private func parser(_ parser: XMLParser, didEndElement elementName: String!, namespaceURI: String!, qualifiedName qName: String!) {
+        if !foundCharacters.isEmpty {
+            
+            if elementName == "link"{
+                foundCharacters = (foundCharacters as NSString).substring(from: 3)
+            }
+            
+            currentDataDictionary[currentElement] = foundCharacters
+            
+            foundCharacters = ""
+            
+            if currentElement == "pubDate" {
+                arrParsedData.append(currentDataDictionary)
+            }
+        }
+    }
+    
+    
+    private func parser(_ parser: XMLParser, foundCharacters string: String!) {
+        if (currentElement == "title" && currentElement != "Appcoda") || currentElement == "link" || currentElement == "pubDate"{
+            foundCharacters += string
+        }
+    }
+    
+    
 }
