9a394c37f7a72a74e0bdf1212494b462143d1553
Branson Boggia <bmanboggia@yahoo.com>
2016-11-14 13:51:14 -0500

    11/14 added working image scroller need debuging tho

--- TCTC Student/HomePageViewController.swift
+++ TCTC Student/HomePageViewController.swift
@@ -10,74 +10,58 @@ import UIKit
 
 class HomePageViewController: UIViewController, UIScrollViewDelegate {
     
-    @IBOutlet weak var mainScrollView: UIScrollView!
-    @IBOutlet var scrollView: UIScrollView!
-    @IBOutlet var textView: UITextView!
-    @IBOutlet var pageControl: UIPageControl!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageScroller: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
     
-    @IBOutlet var startButton: UIButton!
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        mainScrollView.contentInset = UIEdgeInsetsMake(0, 0, 400, 0)
         
-        // Do any additional setup after loading the view, typically from a nib.
-        //1
-        self.scrollView.frame = CGRect(x:0, y:0, width:self.view.frame.width, height:self.view.frame.height)
-        let scrollViewWidth:CGFloat = self.scrollView.frame.width
-        let scrollViewHeight:CGFloat = self.scrollView.frame.height
-        //2
-        textView.textAlignment = .center
-        textView.text = "Sweettutos.com is your blog of choice for Mobile tutorials"
-        textView.textColor = UIColor.black
-        self.startButton.layer.cornerRadius = 4.0
-        //3
-        let imgOne = UIImageView(frame: CGRect(x:0, y:0,width:scrollViewWidth, height:scrollViewHeight))
-        imgOne.image = UIImage(named: "Slide 1")
-        let imgTwo = UIImageView(frame: CGRect(x:scrollViewWidth, y:0,width:scrollViewWidth, height:scrollViewHeight))
-        imgTwo.image = UIImage(named: "Slide 2")
-        let imgThree = UIImageView(frame: CGRect(x:scrollViewWidth*2, y:0,width:scrollViewWidth, height:scrollViewHeight))
+        scrollView.contentInset = UIEdgeInsetsMake(0, 0, 400, 0)
+        
+        self.imageScroller.frame = CGRect(x:0, y:112, width:self.imageScroller.frame.width, height:250)
+        let imageScrollerWidth:CGFloat = self.imageScroller.frame.width
+        let imageScrollerHeight:CGFloat = self.imageScroller.frame.height
+        
+        let imgOne = UIImageView(frame: CGRect(x:imageScrollerWidth*0, y:self.imageScroller.frame.minY,width:imageScrollerWidth, height:imageScrollerHeight))
+        imgOne.image = UIImage(named: "Screen Shot 2016-10-24 at 12.44.07 PM")
+        let imgTwo = UIImageView(frame: CGRect(x:imageScrollerWidth*1, y:self.imageScroller.frame.minY,width:imageScrollerWidth, height:imageScrollerHeight))
+        imgTwo.image = UIImage(named: "logo")
+        let imgThree = UIImageView(frame: CGRect(x:imageScrollerWidth*2, y:self.imageScroller.frame.minY,width:imageScrollerWidth, height:imageScrollerHeight))
         imgThree.image = UIImage(named: "Slide 3")
-        let imgFour = UIImageView(frame: CGRect(x:scrollViewWidth*3, y:0,width:scrollViewWidth, height:scrollViewHeight))
+        let imgFour = UIImageView(frame: CGRect(x:imageScrollerWidth*3, y:self.imageScroller.frame.minY,width:imageScrollerWidth, height:imageScrollerHeight))
         imgFour.image = UIImage(named: "Slide 4")
         
-        self.scrollView.addSubview(imgOne)
-        self.scrollView.addSubview(imgTwo)
-        self.scrollView.addSubview(imgThree)
-        self.scrollView.addSubview(imgFour)
-        //4
-        self.scrollView.contentSize = CGSize(width:self.scrollView.frame.width * 4, height:self.scrollView.frame.height)
-        self.scrollView.delegate = self
+        self.imageScroller.addSubview(imgOne)
+        self.imageScroller.addSubview(imgTwo)
+        self.imageScroller.addSubview(imgThree)
+        self.imageScroller.addSubview(imgFour)
+        
+        self.imageScroller.contentSize = CGSize(width:self.scrollView.frame.width * 4, height:self.scrollView.frame.height)
+        self.imageScroller.delegate = self
         self.pageControl.currentPage = 0
+        
+        Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(moveToNextPage), userInfo: nil, repeats: true)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    //MARK: UIScrollView Delegate
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView){
-        // Test the offset and calculate the current page after scrolling ends
-        let pageWidth:CGFloat = scrollView.frame.width
-        let currentPage:CGFloat = floor((scrollView.contentOffset.x-pageWidth/2)/pageWidth)+1
-        // Change the indicator
-        self.pageControl.currentPage = Int(currentPage);
-        // Change the text accordingly
-        if Int(currentPage) == 0{
-            textView.text = "page 0 text"
-        }else if Int(currentPage) == 1{
-            textView.text = "page 1 text"
-        }else if Int(currentPage) == 2{
-            textView.text = "page 3 text"
-        }else{
-            textView.text = "any other page"
-            // Show the "Let's Start" button in the last slide (with a fade in animation)
-            UIView.animate(withDuration: 1.0, animations: { () -> Void in
-                self.startButton.alpha = 1.0
-            })
+    func moveToNextPage (){
+        
+        let pageWidth:CGFloat = self.scrollView.frame.width
+        let maxWidth:CGFloat = pageWidth * 4
+        let contentOffset:CGFloat = self.scrollView.contentOffset.x
+        
+        var slideToX = contentOffset + pageWidth
+        
+        if  contentOffset + pageWidth == maxWidth
+        {
+            slideToX = 0
         }
+        self.imageScroller.scrollRectToVisible(CGRect(x:slideToX, y:0, width:pageWidth, height:self.imageScroller.frame.height), animated: true)
     }
-
+    
 }
