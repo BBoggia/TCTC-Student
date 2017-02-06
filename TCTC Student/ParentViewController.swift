d9f0e0b990197d9fcf0b890ebcfcaae840b78220
Branson Boggia <bmanboggia@yahoo.com>
2017-02-06 13:53:32 -0500

    2/6/17 all pages just about done now need to start finishing empty buttons and links

---
+++ TCTC Student/ParentViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ParentViewController.swift
+//  TCTC Student
+//
+//  Created by Branson Boggia on 2/6/17.
+//  Copyright © 2017 Pinetree Studios. All rights reserved.
+//
+
+import UIKit
+
+class ParentViewController: UIViewController {
+
+    var strings:[String] = []
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var bulletLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollView.contentInset = UIEdgeInsetsMake(0, 0, 300, 0)
+        
+        let bullet1 = "Allow parents secure access to view their child’s academic progress of grades and assignments"
+        let bullet2 = "Easily view daily attendance, homework, teacher web pages, student schedule and more"
+        let bullet3 = "View teacher comments"
+        let bullet4 = "Parents have the ability to view all their children’s progress from a single account login"
+        let bullet5 = "Easy access to contact information allows parents and teachers to readily communicate"
+        
+        strings = [bullet1, bullet2, bullet3, bullet4, bullet5]
+        
+        let attributesDictionary = [NSFontAttributeName : bulletLabel.font]
+        let fullAttributedString = NSMutableAttributedString(string: "", attributes: attributesDictionary)
+        
+        for string: String in strings
+        {
+            let bulletPoint: String = "\u{2022}"
+            let formattedString: String = "\(bulletPoint) \(string)\n"
+            let attributedString: NSMutableAttributedString = NSMutableAttributedString(string: formattedString)
+            
+            let paragraphStyle = createParagraphAttribute()
+            attributedString.addAttributes([NSParagraphStyleAttributeName: paragraphStyle], range: NSMakeRange(0, attributedString.length))
+            
+            fullAttributedString.append(attributedString)
+        }
+        
+        bulletLabel.attributedText = fullAttributedString
+    }
+    
+    func createParagraphAttribute() ->NSParagraphStyle
+    {
+        var paragraphStyle: NSMutableParagraphStyle
+        paragraphStyle = NSParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
+        paragraphStyle.tabStops = [NSTextTab(textAlignment: .left, location: 15, options: NSDictionary() as! [String : AnyObject])]
+        paragraphStyle.defaultTabInterval = 15
+        paragraphStyle.firstLineHeadIndent = 0
+        paragraphStyle.headIndent = 15
+        
+        return paragraphStyle
+    }
+
+}
